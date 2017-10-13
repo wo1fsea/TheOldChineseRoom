daeae889c5dad045cd93752686d43c635e03c48d
Quanyong Huang <quanyongh@foxmail.com>
2017-10-13 14:08:11 +0800

    add: fps

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -10,51 +10,59 @@ Description:
 ----------------------------------------------------------------------------"""
 
 import sys
-from PIL import Image
+import time
+from PIL import Image, ImageDraw
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
 from PyQt5.QtCore import QTimer
 
 from core.communication.queue import Queue
 from core.config_reader import ConfigReader
 
 
-class App(QWidget):
-    def __init__(self):
-        super(App, self).__init__()
-        self.title = 'monitor'
-        self.left = 0
-        self.top = 0
-        self.width = 640
-        self.height = 480
-        self.init()
+class App(QMainWindow):
+	def __init__(self):
+		super(App, self).__init__()
+		self.title = 'monitor'
+		self.left = 0
+		self.top = 0
+		self.width = 640
+		self.height = 480
+		self.init()
 
-        config = ConfigReader().get_config("frame_capture")
-        self._queue = Queue(config["frame_cache_key"], config["frame_cache_length"])
+		config = ConfigReader().get_config("frame_capture")
+		self._queue = Queue(config["frame_cache_key"], config["frame_cache_length"])
 
-    def init(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+		self._last_time = 0
 
-        # Create widget
-        self.label = QLabel(self)
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.timeOut)
-        self.timer.start(10)
-        self.show()
+	def init(self):
+		self.setWindowTitle(self.title)
 
-    def timeOut(self):
-        data = self._queue.get()
-        if data:
-            size, bytes = eval(data)
-            frame = Image.frombytes('RGB', size, bytes)
-            pixmap = frame.toqpixmap()
-            self.label.setPixmap(pixmap)
-            self.label.resize(pixmap.width(), pixmap.height())
-            self.resize(pixmap.width(), pixmap.height())
+		# Create widget
+		self.label = QLabel(self)
+		self.timer = QTimer(self)
+		self.timer.timeout.connect(self.timeOut)
+		self.timer.start(10)
+		self.show()
+
+	def timeOut(self):
+		data = self._queue.get()
+		if data:
+			size, bytes = eval(data)
+			frame = Image.frombytes('RGB', size, bytes)
+			cur_time = time.time()
+			interval = cur_time - self._last_time
+			self._last_time = cur_time
+			self.setWindowTitle("fps: %f" % (1 / interval))
+			# draw = ImageDraw.Draw(frame)
+			# draw.text((10, 10), "fps: %f" % (1 / interval), fill=(255, 255, 255, 128))
+			pixmap = frame.toqpixmap()
+			self.label.setPixmap(pixmap)
+			self.label.resize(pixmap.width(), pixmap.height())
+			self.resize(pixmap.width(), pixmap.height())
 
 
 def start():
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
