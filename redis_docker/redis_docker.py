5db1c70edbaba11118bebe993b0b3e42b4138188
Quanyong Huang <wo1fsea@126.com>
2017-10-28 23:29:19 +0800

    mod: redis_docker

--- redis_docker/redis_docker.py
+++ redis_docker/redis_docker.py
@@ -15,10 +15,10 @@ import docker
 def start():
     client = docker.from_env()
     # client.containers.
-    redis_docker = client.containers.get("redis")
-    if not redis_docker:
+    try:
+        redis_docker = client.containers.get("redis")
+    except docker.errors.NotFound as nfe:
         client.containers.run("redis", name="redis", ports={"6379/tcp": 6379}, detach=True)
     else:
         if redis_docker.status != "running":
             redis_docker.start()
-
